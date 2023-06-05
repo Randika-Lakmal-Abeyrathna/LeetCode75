86f24ee36b14551d3ed1d4e91b4bdead38d3d011
Randika Lakmal <randika.l.abeyrathna@gmail.com>
2023-06-05 10:47:14 +0530

    1431. Kids With the Greatest Number of Candies
    Submitted the solution

---
+++ KidsWiththeGreatestNumberofCandies.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+//Input: candies = [2,3,5,1,3], extraCandies = 3
+//        Output: [true,true,true,false,true]
+//        Explanation: If you give all extraCandies to:
+//        - Kid 1, they will have 2 + 3 = 5 candies, which is the greatest among the kids.
+//        - Kid 2, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+//        - Kid 3, they will have 5 + 3 = 8 candies, which is the greatest among the kids.
+//        - Kid 4, they will have 1 + 3 = 4 candies, which is not the greatest among the kids.
+//        - Kid 5, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+
+public class KidsWiththeGreatestNumberofCandies {
+
+
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies){
+
+        List<Boolean> results = new ArrayList<>();
+        int[] copy = candies.clone();
+        Arrays.sort(copy);
+
+        int Max = copy[copy.length-1];
+
+        for (int i = 0; i < candies.length; i++) {
+            int curr=candies[i];
+
+            if (Max <= curr + extraCandies){
+                results.add(true);
+            }else{
+                results.add(false);
+            }
+        }
+
+        return results;
+    }
+
+    public static void main(String[] args) {
+        int[] c1 = {2,3,5,1,3};
+        int ex1 =3;
+
+        System.out.println(kidsWithCandies(c1,ex1));
+    }
+
+}
