cb1041d21900ea29f04ced08eb87bb393da34e2e
Randika Lakmal <randika.l.abeyrathna@gmail.com>
2023-07-11 22:50:51 +0530

    104. Maximum Depth of Binary Tree
    Solution accepted

---
+++ BinaryTree_DFS/MaximumDepthofBinaryTree.java
@@ -0,0 +1,36 @@
+package BinaryTree_DFS;
+
+public class MaximumDepthofBinaryTree {
+
+
+    public int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return Math.max(left, right) + 1;
+    }
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
